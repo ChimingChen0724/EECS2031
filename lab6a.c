e71e2ec1892532f3cf5c528d8ab06d5659fa7e56
alex <alex.chen-724@gmail.com>
2020-07-08 16:07:16 -0400

    first version

---
+++ lab6a.c
@@ -0,0 +1,26 @@
+/***************************************
+
+EECS2031 – Lab 6
+
+Filename:	lab6a.c
+
+Author: Chiming Chen
+
+Email: alex.chen0724@gmail.com
+
+EECS login ID: alex0724
+
+****************************************/
+
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    float inch, cm;
+    while ( inch != EOF ){
+    printf("Enter the measurement in inches>");
+    scanf("%f", &inch);
+    cm = inch * 2.54;
+    printf("%.2f cm\n", cm);
+    }
+}
---
+++ lab6b.c
@@ -0,0 +1,30 @@
+/***************************************
+
+EECS2031 – Lab 6
+
+Filename:	lab6b.c
+
+Author: Chiming Chen
+
+Email: alex.chen0724@gmail.com
+
+EECS login ID: alex0724
+
+****************************************/
+
+#include<stdio.h>
+#define MAXLINE 1000
+int main()
+    {
+        int i , space;
+        char str[MAXLINE];
+        while ( space != EOF ){
+        printf("Enter a line of characters>");
+        fgets(str, MAXLINE, stdin);
+        for(i=0;str[i];i++)
+            if(str[i] == ' ')
+                    space++;
+        printf("%d\n", space);
+        space=0;
+        }
+    }
